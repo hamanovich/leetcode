8d1859898ac07947e5d3f9e195646e4b5040a24f
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-06-20 16:17:54 +0300

    [Common]: Stack

---
+++ src/common/stack/stack.js
@@ -0,0 +1,28 @@
+export class Stack {
+  #items = [];
+
+  push(element) {
+    this.#items.push(element);
+  }
+
+  pop() {
+    if (!this.#items.length) throw new RangeError('Underflow');
+    return this.#items.pop();
+  }
+
+  isEmpty() {
+    return this.#items.length === 0;
+  }
+
+  empty() {
+    return (this.#items.length = 0);
+  }
+
+  size() {
+    return this.#items.length;
+  }
+
+  peek() {
+    return this.#items[this.#items.length - 1];
+  }
+}
