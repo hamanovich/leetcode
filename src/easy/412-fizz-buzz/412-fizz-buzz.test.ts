b256edb916bfb4b248e76e2121f10582643a00b2
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-06-26 17:30:21 +0300

    [Easy]: 412-fizz-buzz

---
+++ src/easy/412-fizz-buzz/412-fizz-buzz.test.ts
@@ -0,0 +1,32 @@
+import { fizzBuzz } from './412-fizz-buzz';
+
+describe('fizzBuzz', () => {
+  test('n = 3', () => {
+    console.log('fi', fizzBuzz(3));
+    expect(fizzBuzz(3)).toStrictEqual(['1', '2', 'Fizz']);
+  });
+
+  test('n = 5', () => {
+    expect(fizzBuzz(5)).toStrictEqual(['1', '2', 'Fizz', '4', 'Buzz']);
+  });
+
+  test('n = 15', () => {
+    expect(fizzBuzz(15)).toStrictEqual([
+      '1',
+      '2',
+      'Fizz',
+      '4',
+      'Buzz',
+      'Fizz',
+      '7',
+      '8',
+      'Fizz',
+      'Buzz',
+      '11',
+      'Fizz',
+      '13',
+      '14',
+      'FizzBuzz',
+    ]);
+  });
+});
