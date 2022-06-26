6510781f6fde4c9bf792f04c6a8693686f00d5fe
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-06-26 19:15:52 +0300

    [Easy]: 202-happy-number. Typos

--- src/easy/202-happy-number/202-happy-number.ts
+++ src/easy/202-happy-number/202-happy-number.ts
@@ -1,20 +1,16 @@
 export const isHappy = (n: number): boolean => {
   const squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81];
-  let set = new Set();
+  const set = new Set();
 
   while (!set.has(n)) {
     set.add(n);
     let s = n.toString();
     n = 0;
 
-    for (let i = 0; i < s.length; ++i) {
-      n += squares[Number(s[i])];
-    }
+    for (let i = 0; i < s.length; ++i) n += squares[Number(s[i])];
 
     if (n === 1) return true;
   }
 
   return false;
 };
-
-isHappy(7);
