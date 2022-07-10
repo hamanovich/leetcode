c0a8c05f5a9650060461acf7780c45e216ef7a3b
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-07-10 14:13:24 +0300

    [Easy]: 257-binary-tree-paths

---
+++ src/easy/257-binary-tree-paths/257-binary-tree-paths.test.ts
@@ -0,0 +1,18 @@
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+import { binaryTreePaths } from './257-binary-tree-paths';
+
+describe('binaryTreePaths', () => {
+  test('Input: root = [1,2,3,null,5]; Output: ["1->2->5","1->3"]', () => {
+    const tree = new BinaryTreeNode(1);
+    tree.left = new BinaryTreeNode(2);
+    tree.right = new BinaryTreeNode(3);
+    tree.left.right = new BinaryTreeNode(5);
+
+    expect(binaryTreePaths(tree)).toStrictEqual(['1->2->5', '1->3']);
+  });
+
+  test('root = [1]; Output: ["1"]', () => {
+    const tree = new BinaryTreeNode(1);
+    expect(binaryTreePaths(tree)).toStrictEqual(['1']);
+  });
+});
---
+++ src/easy/257-binary-tree-paths/257-binary-tree-paths.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/binary-tree-paths/
+
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+
+export const binaryTreePaths = (root: BinaryTreeNode | null): string[] => {
+  const dfs = (root: BinaryTreeNode | null, path: string): string[] => {
+    if (!root) return [];
+
+    path += root.val;
+    if (!root.left && !root.right) return [path];
+    path += '->';
+
+    return dfs(root.left, path).concat(dfs(root.right, path));
+  };
+
+  return dfs(root, '');
+};
