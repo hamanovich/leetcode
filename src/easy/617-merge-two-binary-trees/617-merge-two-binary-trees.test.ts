481ece93618e5e7d03953613213673cd195cf212
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-07-10 15:18:40 +0300

    [Easy]: 617-merge-two-binary-trees

---
+++ src/easy/617-merge-two-binary-trees/617-merge-two-binary-trees.test.ts
@@ -0,0 +1,54 @@
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+import { mergeTrees } from './617-merge-two-binary-trees';
+
+describe('mergeTrees', () => {
+  test('Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]; Output: [3,4,5,5,4,null,7]', () => {
+    const tree1 = new BinaryTreeNode(1);
+    tree1.left = new BinaryTreeNode(3);
+    tree1.right = new BinaryTreeNode(2);
+    tree1.left.left = new BinaryTreeNode(5);
+
+    const tree2 = new BinaryTreeNode(2);
+    tree2.left = new BinaryTreeNode(1);
+    tree2.right = new BinaryTreeNode(3);
+    tree2.left.right = new BinaryTreeNode(4);
+    tree2.right.right = new BinaryTreeNode(7);
+
+    const treeResult = new BinaryTreeNode(3);
+    treeResult.left = new BinaryTreeNode(4);
+    treeResult.right = new BinaryTreeNode(5);
+    treeResult.left.left = new BinaryTreeNode(5);
+    treeResult.left.right = new BinaryTreeNode(4);
+    treeResult.right.right = new BinaryTreeNode(7);
+
+    expect(mergeTrees(tree1, tree2)).toStrictEqual(treeResult);
+  });
+
+  test('Input: root1 = [1], root2 = [1,2]; Output: [2,2]', () => {
+    const tree1 = new BinaryTreeNode(1);
+
+    const tree2 = new BinaryTreeNode(1);
+    tree2.left = new BinaryTreeNode(2);
+
+    const treeResult = new BinaryTreeNode(2);
+    treeResult.left = new BinaryTreeNode(2);
+
+    expect(mergeTrees(tree1, tree2)).toStrictEqual(treeResult);
+  });
+
+  test('Input: root1 = [], root2 = []; Output: null', () => {
+    expect(mergeTrees()).toBeNull();
+  });
+
+  test('Input: root1 = [], root2 = [1]; Output: [1]', () => {
+    const tree2 = new BinaryTreeNode(1);
+
+    expect(mergeTrees(undefined, tree2)).toStrictEqual(tree2);
+  });
+
+  test('Input: root1 = [1], root2 = [0]; Output: [1]', () => {
+    const tree1 = new BinaryTreeNode(1);
+
+    expect(mergeTrees(tree1)).toStrictEqual(tree1);
+  });
+});
---
+++ src/easy/617-merge-two-binary-trees/617-merge-two-binary-trees.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+
+export const mergeTrees = (root1?: BinaryTreeNode | null, root2?: BinaryTreeNode | null): BinaryTreeNode | null => {
+  if (!root1 && !root2) return null;
+  if (!root1) return root2 as BinaryTreeNode;
+  if (!root2) return root1;
+
+  const newRoot = new BinaryTreeNode((root1.val as number) + (root2.val as number));
+  newRoot.left = mergeTrees(root1.left, root2.left);
+  newRoot.right = mergeTrees(root1.right, root2.right);
+
+  return newRoot;
+};
