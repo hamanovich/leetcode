c16c869220eb023ab1a0300182fc48969f8357a3
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-07-19 20:21:57 +0300

    [Medium]: 109-convert-sorted-list-to-binary-search-tree

---
+++ src/medium/109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.test.ts
@@ -0,0 +1,29 @@
+import { LinkedListNode } from '../../common/linked-list/linked-list-node';
+import { LinkedList } from '../../common/linked-list/linked-list';
+import { sortedListToBST } from './109-convert-sorted-list-to-binary-search-tree';
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+
+describe('sortedListToBST', () => {
+  test('Input: head = [-10,-3,0,5,9]; Output: [0,-3,9,-10,null,5]', () => {
+    const list = new LinkedList();
+    const tree = new BinaryTreeNode(0);
+
+    tree.left = new BinaryTreeNode(-10);
+    tree.right = new BinaryTreeNode(5);
+    tree.left.right = new BinaryTreeNode(-3);
+    tree.right.right = new BinaryTreeNode(9);
+
+    list.append(-10);
+    list.append(-3);
+    list.append(0);
+    list.append(5);
+    list.append(9);
+
+    expect(sortedListToBST(list.head as LinkedListNode<number>)).toEqual(tree);
+  });
+
+  test('Input: head = []; Output: []', () => {
+    const list = new LinkedList();
+    expect(sortedListToBST(list.head as LinkedListNode<number>)).toBeNull();
+  });
+});
---
+++ src/medium/109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.ts
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+
+import { LinkedListNode } from '../../common/linked-list/linked-list-node';
+import { BinaryTreeNode } from '../../common/tree/binary-tree-node';
+
+export const sortedListToBST = (head: LinkedListNode<number> | null): BinaryTreeNode<number> | null => {
+  let curr = head;
+  let count = 0;
+
+  while (curr) {
+    curr = curr.next;
+    count++;
+  }
+
+  const traverse = (i: number, j: number): BinaryTreeNode<number> | null => {
+    if (j < i) return null;
+
+    const middle = (i + j) >> 1;
+    const node = new BinaryTreeNode<number>();
+
+    node.left = traverse(i, middle - 1);
+    node.val = curr?.value;
+    curr = curr?.next as LinkedListNode<number>;
+    node.right = traverse(middle + 1, j);
+
+    return node;
+  };
+
+  curr = head;
+
+  return traverse(1, count);
+};
