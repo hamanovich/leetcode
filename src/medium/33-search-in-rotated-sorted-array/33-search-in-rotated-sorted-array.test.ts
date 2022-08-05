a3cb9f5c42cba9bcaca450bb3d611461a374b5b2
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-08-05 16:53:14 +0300

    [Medium]: 33-search-in-rotated-sorted-array. Improve test

--- src/medium/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.test.ts
+++ src/medium/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.test.ts
@@ -5,6 +5,18 @@ describe('search', () => {
     expect(search([4, 5, 6, 7, 0, 1, 2], 0)).toBe(4);
   });
 
+  test('Input: nums = [4,5,6,7,0,1,2], target = 5; Output: 1', () => {
+    expect(search([4, 5, 6, 7, 0, 1, 2], 5)).toBe(1);
+  });
+
+  test('Input: nums = [7,0,1,2], target = 2; Output: 3', () => {
+    expect(search([7, 0, 1, 2], 2)).toBe(3);
+  });
+
+  test('Input: nums = [7,0,1,2], target = 7; Output: 0', () => {
+    expect(search([7, 0, 1, 2], 7)).toBe(0);
+  });
+
   test('Input: nums = [4,5,6,7,0,1,2], target = 3; Output: -1', () => {
     expect(search([4, 5, 6, 7, 0, 1, 2], 3)).toBe(-1);
   });
